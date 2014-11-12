93ff1936e7466eb387094034f675193327be2bb5
Martin Halamíček <martin@halamicek.cz>
2014-11-12 17:18:41 +0100

    build tools update

--- karma.conf.js
+++ karma.conf.js
@@ -1,58 +1,59 @@
+
+
+
 // Karma configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+// Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
 
-// list of files / patterns to load in the browser
-files = [
-  	JASMINE,
-  	JASMINE_ADAPTER,
-	'docs/components/jquery/jquery.js',
-	'docs/components/bootstrap/docs/assets/js/tooltip.js',
-    'docs/components/underscore/underscore.js',
-	'docs/components/angular/angular.js',
-	'docs/components/angular-mocks/angular-mocks.js',
-	'docs/components/angular-route/angular-route.js',
-  	'docs/build/angular-kb.js',
-  	'tmp/modules/**/test/*.js'
-];
+module.exports = function(config) {
+    config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-// list of files to exclude
-exclude = [];
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['dots'];
+        // list of files / patterns to load in the browser
+        files: [
+            'docs/components/jquery/jquery.js',
+            'docs/components/bootstrap/docs/assets/js/tooltip.js',
+            'docs/components/underscore/underscore.js',
+            'docs/components/angular/angular.js',
+            'docs/components/angular-mocks/angular-mocks.js',
+            'docs/components/angular-route/angular-route.js',
+            'docs/build/angular-kb.js',
+            'tmp/modules/**/test/*.js'
+        ],
 
-// web server port
-port = 8080;
+        // list of files / patterns to exclude
+        exclude: [],
 
-// cli runner port
-runnerPort = 9100;
+        // web server port
+        port: 8080,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
+};
